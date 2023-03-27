cece40ae8c35373ac3a11d67c2c4d4b7702a2bcb
rovenssiren9417 <lzw_roven@live.cn>
2023-03-27 10:28:36 +0800

    style: bufferline去除了花里胡哨的设置

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,21 +1,21 @@
-
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-  view = {
-    hide_root_folder = true,
-    float = {
-      enable = true,
-      open_win_config = {
-       relative='editor',
-        border = 'rounded',
-        width=100,height=100
-      }
-    }
-  }
+	sort_by = "case_sensitive",
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	view = {
+		hide_root_folder = true,
+		float = {
+			enable = true,
+			open_win_config = {
+				relative = "editor",
+				border = "rounded",
+				width = 100,
+				height = 100,
+			},
+		},
+	},
 })
