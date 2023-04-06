bf89e781c4b58e6f7a0541807973c2c7b9d40b83
rovenssiren9417 <lzw_roven@live.cn>
2023-04-06 14:37:31 +0800

    feat: 更换了开始界面为veil, 添加了窗口动画

---
+++ lua/plugins/veil.lua
@@ -0,0 +1,9 @@
+return {
+	"willothy/veil.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
+	},
+	config = true,
+}
