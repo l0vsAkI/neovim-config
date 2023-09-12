5b5075ceac0c4435bc1cb538d63341a50f2746fd
lzw_roven@live.cn <lzw_roven@live.cn>
2023-09-12 13:48:16 +0800

    fix: 取消noice的lsp进度展示

--- lua/plugins/buildin-config.lua
+++ lua/plugins/buildin-config.lua
@@ -51,8 +51,8 @@ return {
   {
     "akinsho/bufferline.nvim",
     keys = {
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",  desc = "Close right buffers" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",   desc = "Close left buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Close right buffers" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Close left buffers" },
       { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close left buffers" },
     },
     opts = {
@@ -163,7 +163,6 @@ return {
   {
     "williamboman/mason.nvim",
     opts = {
-      ensure_installed = { "vue-language-server" },
       ui = {
         icons = {
           package_installed = "✓",
@@ -178,5 +177,34 @@ return {
   {
     "SmiteshP/nvim-navic",
     cond = false,
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        progress = {
+          enabled = false,
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+    },
   },
 }
