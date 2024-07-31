fa826f67536104ab9c5f50b0afa3a2aeedd7d48e
lzw_roven@live.cn <lzw_roven@live.cn>
2024-07-31 10:08:54 +0800

    chore:

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -20,9 +20,6 @@ return {
   -- Editor UI
   {
     "lukas-reineke/indent-blankline.nvim",
-    opts = {
-      indent = { char = "" },
-    },
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -47,59 +44,6 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
-    opts = function(_)
-      local cyberdream = require("lualine.themes.cyberdream").get_theme() -- as of v2.0.0. For older versions, use `require("lualine.themes.cyberdream")`
-      local icons = require("lazyvim.config").icons
-      local function fg(name)
-        return function()
-          ---@type {foreground?:number}?
-          local hl = vim.api.nvim_get_hl_by_name(name, true)
-          return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
-        end
-      end
-      return {
-        options = {
-          theme = cyberdream, -- theme = "auto"
-          globalstatus = true,
-          disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = {
-            {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
-            },
-          },
-          lualine_x = {
-            { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Special") },
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-            },
-          },
-          lualine_y = {
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 1 } },
-          },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
-          },
-        },
-        extensions = { "neo-tree" },
-      }
-    end,
   },
 
   -- treesitter
